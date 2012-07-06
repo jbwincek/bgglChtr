732fbbbd0dc5dac6a3f0aa5ff1d75cc7c5398107
J B Wincek <jbwincek@gmail.com>
2012-07-06 11:43:32 -0400

    Accurately* generates the dictionary
    * I think it generates all of them
    Also there is a ton of test code

--- dictionary_generator.py
+++ dictionary_generator.py
@@ -1,4 +1,5 @@
 import cProfile 
+import random
 
 def remove_shorts(word_list,minimum_length):
 	"""Remove words shorter than minimum_length, return the modified list."""
@@ -42,7 +43,7 @@ def build_the_dictionary(word_list,minimum_length,max_length):
 			#print(' %s' % word),
 			the_dict[word]=False
 			counter += 1
-	print ('Added a total of %d words.' % counter)
+	print ('Regular BtD attempted to add a total of %d words. %d words were actually added' % (counter,len(the_dict)))
 	return the_dict
 
 def quicker_build_the_dictionary(word_list,minimum_length,max_length):
@@ -54,25 +55,44 @@ def quicker_build_the_dictionary(word_list,minimum_length,max_length):
 		for word in list_without_shorts:
 			the_dict[word[0:cur_len]] = False
 			counter+=1
-	print ('Added a total of %d words.' % counter)
+	print ('Quicker BtD attempted to add a total of %d words. %d words were actually added' % (counter,len(the_dict)))
 	return the_dict	
 
-def extract_a_random_sampling(word_list,how_many)
+def extract_a_random_sampling(word_list,how_many):
+	working_list = []
+	for i in range(0,how_many):
+		working_list.append(random.choice(word_list))
+	return working_list
 
-def run(where,min_length,max_length):
-	word_list_txt_ob = open(where)
-	word_list_txt = word_list_txt_ob.read()
-	word_list = word_list_txt.split()
+def compare(): pass
+	
+def run(which_func, word_list, min_length, max_length):
+	"""Run the specified dictionary build with the word list using only the given lengths."""
+	
+	#some testing functions
 	#word_list = word_list[0:800]
 	#word_list = remove_shorts(word_list,3)
 	#three_long =  extract_first_letters(word_list,3)
 	#print(word_list)
 	#print(three_long)
 	#print(the_dict)
-	the_dict = build_the_dictionary(word_list,min_length,max_length)
-	
-cProfile.run('run("boggle_dictionary.txt",3,13)')
+	the_dict = which_func(word_list, min_length, max_length)
+	print the_dict
+
+
+
+word_list_txt_ob = open("boggle_dictionary.txt")
+word_list_txt = word_list_txt_ob.read()
+word_list = word_list_txt.split()
+
+random_sample = extract_a_random_sampling(word_list,5)
+
+#cProfile.run('run(build_the_dictionary,random_sample,3,13)')
+#cProfile.run('run(quicker_build_the_dictionary,random_sample,3,13)')
 #run("boggle_dictionary.txt",3,13)
+print ( 'random_sample is %d words long and is:\n %s' % (len(random_sample),str(random_sample)))
+run(quicker_build_the_dictionary,random_sample,3,13)
+run(build_the_dictionary,random_sample,3,13)
 
 
 
