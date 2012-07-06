23b63f0541811ffe84c6085fbb3e30e76cb65c19
J B Wincek <jbwincek@gmail.com>
2012-07-06 02:47:15 -0400

    started drafting the control flow diagram

---
+++ control_flow.py
@@ -0,0 +1,19 @@
+"""
+bgglChtr control flow 
+
+start the program 
+  * load the dictionary 
+  * main loop 
+    * prompt the user for the board configuration 
+    * prompt for maximum search depth 
+    * search using the configuration
+      * create a list of all the three letter possible combinations 
+        * exclude loop backs 
+    
+    
+    
+search(configuration)
+  * 
+
+
+"""
